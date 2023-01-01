8b699bf1d2d2e30f5185b65ad99bf1d0e31f639e
Abhiraj Ghosh <21bca1058@cuchd.in>
2023-01-02 02:54:31 +0530

    final deploy

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from the server
-  const response = await fetch('http://localhost:5000',
+  const response = await fetch('https://talkgpt-by-abhiraj-g.onrender.com/',
   {
     method: 'POST',
     headers:{
